9b5c5da35076755a9e3d934e0eca333cd19ca3a3
MistSilv <191048857+MistSilv@users.noreply.github.com>
2025-07-01 10:04:21 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig({
 
         // Konfiguracja PWA
         VitePWA({
+            filename: 'sw.js', // Nazwa pliku Service Workera
             registerType: 'autoUpdate', // Automatyczna aktualizacja Service Workera
 
             includeAssets: [
