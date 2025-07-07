4782bbf40518e3aeab6853affd9951757389f5d2
JustAFailuree <68466522+JustAFailuree@users.noreply.github.com>
2025-07-07 11:45:49 +0200

    iiuyghk

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,15 +1,15 @@
-<!-- strona główna aplikacji z nagłówkiem i nawigacją -->
 <!DOCTYPE html>
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
     <title>{{ $title ?? 'Aplikacja Zamówień' }}</title>
-    <meta name="viewport" content="width=device-width, initial-sale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="manifest" href="/build/manifest.webmanifest">
     <meta name="theme-color" content="#1e293b">
 </head>
 <body class="bg-black">
+    
     <nav class="bg-black px-4 py-3 flex items-center shadow mb-6 relative">
         <div class="flex items-center space-x-4 absolute top-0 left-0 ml-3 mt-3">
             <a href="{{ url('/welcome') }}" class="text-white font-bold text-lg hover:text-blue-900 transition flex items-center">
@@ -25,7 +25,7 @@
     </nav>
     
     <main class="container mx-auto py-6 px-4">
-        {{ $slot }} <!-- miejsce na zawartość strony -->
+        {{ $slot }}
     </main>
 </body>
 </html>
