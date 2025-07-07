37e84afe335cc66d496666dad0c36d6a5f21015a
JustAFailuree <68466522+JustAFailuree@users.noreply.github.com>
2025-07-07 11:45:18 +0200

    qazxcvbnm

--- resources/views/components/layoutL.blade.php
+++ resources/views/components/layoutL.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ $title ?? 'Aplikacja Zamówień' }}</title>
-    <meta name="viewport" content="width=device-width, initial-sale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="manifest" href="/build/manifest.webmanifest">
     <meta name="theme-color" content="#1e293b">
