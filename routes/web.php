23e5e2b3d0a84f108cd997111ef271267b02c5a9
JustAFailuree <68466522+JustAFailuree@users.noreply.github.com>
2025-07-10 09:36:18 +0200

    [--------;';';';';;

--- routes/web.php
+++ routes/web.php
@@ -29,9 +29,10 @@ Route::post('/password/reset', [ResetPasswordController::class, 'reset'])->name(
 Route::middleware(['auth'])->group(function () {
     Route::get('/welcome', [AutomatController::class, 'index'])->name('welcome');
 
-
-    Route::get('/zamowienia/produkcja/nowe', [ZamowienieController::class, 'createProdukcja'])->name('zamowienia.produkcja.create');
+        Route::get('/zamowienia/produkcja/nowe', [ZamowienieController::class, 'createProdukcja'])->name('zamowienia.produkcja.create');
     Route::get('/zamowienia/nowe', [ZamowienieController::class, 'create'])->name('zamowienia.create');
+    Route::post('/zamowienia', [ZamowienieController::class, 'store'])->name('zamowienia.store');
+    Route::resource('zamowienia', ZamowienieController::class)->only(['create', 'store', 'index']);
     Route::get('/zamowienia', [ZamowienieController::class, 'index'])->name('zamowienia.index');
     Route::get('/zamowienia/archiwum', [ZamowienieController::class, 'archiwum'])->name('zamowienia.archiwum');
     Route::get('/zamowienia/{zamowienie}', [ZamowienieController::class, 'show'])->name('zamowienia.show');
@@ -39,13 +40,7 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/zamowienia/podsumowanie/tydzien/{date?}', [ZamowienieController::class, 'podsumowanieTygodnia'])->name('zamowienia.podsumowanie.tydzien');
     Route::get('/zamowienia/podsumowanie/miesiac/{month?}', [ZamowienieController::class, 'podsumowanieMiesiaca'])->name('zamowienia.podsumowanie.miesiac');
     Route::get('/zamowienia/podsumowanie/rok/{year?}', [ZamowienieController::class, 'podsumowanieRoku'])->name('zamowienia.podsumowanie.rok');
-    Route::get('/zamowienia/{id}/xlsx', [ZamowienieController::class, 'pobierzZamowienieXlsx'])->name('zamowienia.xlsx');
-    Route::get('/zamowienia/{id}/csv', [ZamowienieController::class, 'pobierzZamowienieCsv'])->name('zamowienia.csv');
-
-    Route::post('/zamowienia', [ZamowienieController::class, 'store'])->name('zamowienia.store');
-    Route::post('/zamowienie/zloz', [ZamowienieController::class, 'store'])->name('zloz.zamowienie');
-
-    Route::resource('zamowienia', ZamowienieController::class)->only(['create', 'store', 'index']);
+    
 
 
     Route::get('/straty/podsumowanie/dzien/{date?}', [StrataController::class, 'podsumowanieDnia'])->name('straty.podsumowanie.dzien');
@@ -53,36 +48,46 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/straty/podsumowanie/miesiac/{month?}', [StrataController::class, 'podsumowanieMiesiaca'])->name('straty.podsumowanie.miesiac');
     Route::get('/straty/podsumowanie/rok/{year?}', [StrataController::class, 'podsumowanieRoku'])->name('straty.podsumowanie.rok');
 
-    Route::resource('straty', StrataController::class, ['parameters' => ['straty' => 'strata']])->only(['index', 'create', 'store', 'show']);
-
 
     Route::get('/register', [RegisterController::class, 'show'])->name('register');
     Route::post('/register', [RegisterController::class, 'store']);
 
+    Route::resource('straty', StrataController::class, [
+'parameters' => ['straty' => 'strata']
+    ])->only(['index', 'create', 'store', 'show']);
+
  
-    Route::get('/export/zamowienia/{zakres}/{date?}/{format?}', [ExportController::class, 'exportZamowienia'])->name('export.zamowienia');
-    Route::get('/export/straty/{zakres}/{date?}/{format?}', [ExportController::class, 'exportStraty'])->name('export.straty');
+    Route::get('/export/zamowienia/{zakres}/{date?}/{format?}', [ExportController::class, 'exportZamowienia'])
+        ->name('export.zamowienia');
+
+
+    Route::get('/export/straty/{zakres}/{date?}/{format?}', [ExportController::class, 'exportStraty'])
+        ->name('export.straty');
+
     Route::get('/export/zamowienie/{zamowienie_id}/{format}', [ExportController::class, 'exportPojedynczeZamowienie'])->name('export.zamowienie');
 
 
     Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-    
+    Route::resource('wsady', WsadController::class);
     Route::get('/wsady/automat/{automat_id}', [WsadController::class, 'index'])->name('wsady.byAutomat');
     Route::get('/wsady', [WsadController::class, 'create'])->name('wsady.index');
     Route::post('/wsady/{produkt_id}/{automat_id}/decrease', [WsadController::class, 'decrease'])->name('wsady.decrease');
     Route::delete('/wsady/{produkt_id}/{automat_id}/delete', [WsadController::class, 'delete'])->name('wsady.delete');
-    Route::resource('wsady', WsadController::class);
+
+    Route::post('/zamowienie/zloz', [ZamowienieController::class, 'store'])->name('zloz.zamowienie');
+
+    Route::get('/zamowienia/{id}/xlsx', [ZamowienieController::class, 'pobierzZamowienieXlsx'])->name('zamowienia.xlsx');
+    Route::get('/zamowienia/{id}/csv', [ZamowienieController::class, 'pobierzZamowienieCsv'])->name('zamowienia.csv');
 
     
     // Wyświetlenie formularza bez tworzenia zamówienia
     Route::get('/produkty/niewlasne/formularz-nowe-zamowienie', [ProduktController::class, 'formularzNoweZamowienie'])->name('produkty.zamowienie.formularz');
 
+    // Istniejące
     Route::get('/produkty/niewlasne/nowe-zamowienie', [ProduktController::class, 'noweZamowienie'])->name('produkty.zamowienie.nowe');
     Route::get('/produkty/niewlasne/zamowienie/{zamowienieId}', [ProduktController::class, 'edytujZamowienie'])->name('produkty.zamowienie.edytuj');
     Route::post('/produkty/niewlasne/zamowienie/zapisz', [ProduktController::class, 'zapiszZamowienie'])->name('produkty.zamowienie.zapisz');
     
-    
 
 });
-
